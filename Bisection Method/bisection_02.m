73789b542c7d30c58ded448327ae9585175ce600
Shahriar Khan <67644471+Shahriar-k@users.noreply.github.com>
2021-03-24 12:02:27 +0600

    Create bisection_02.m

---
+++ Bisection Method/bisection_02.m
@@ -0,0 +1,20 @@
+xl = 2;
+xu = 3;
+
+xr = (xl+xu)/2;
+
+fxl = 2*xl+0.026*log(xl/10^-12)-5;
+fxu = 2*xl+0.026*log(xu/10^-12)-5;
+fxr = 2*xl+0.026*log(xr/10^-12)-5;
+
+if fxr*fxu < 0
+    xl = xr;
+else
+    xu = xr;
+end
+
+xr_new = (xl+xu)/2;
+
+error = abs((xr_new - xr)/xr)*100;
+
+    
