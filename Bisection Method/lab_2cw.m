7591814d1f15fb77e1d8429c2cd6bf51c4901d02
Shahriar Khan <67644471+Shahriar-k@users.noreply.github.com>
2021-03-24 08:56:23 +0600

    Update lab_2cw.m

--- Bisection Method/lab_2cw.m
+++ Bisection Method/lab_2cw.m
@@ -1,14 +1,14 @@
 ep_s = 1e-3;
 error = 1;
-xl = 3;
+xl = 2;
 xu = 3;
 
 while error > ep_s
    xr = (xl+xu)/2;
    
-   fxl = 2*xl + 0.026*ln(xl/10^-12) -5;
-   fxu = 2*xu + 0.026*ln(xu/10^-12) -5;
-   fxr = 2*xl + 0.026*ln(xr/10^-12) -5;
+   fxl = 2*xl + 0.026*log(xl/10^-12) -5;
+   fxu = 2*xu + 0.026*log(xu/10^-12) -5;
+   fxr = 2*xl + 0.026*log(xr/10^-12) -5;
    
    
 end
